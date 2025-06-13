3bb2a0d98fbdcafabcb52cb01a2b3a82923b2c30
Tanay <tanayv05@gmail.com>
2025-06-12 23:52:10 -0400

    finished filtering passing data and filtered receiving data

--- data_extraction.py
+++ data_extraction.py
@@ -68,6 +68,7 @@ de_df = de_df[de_df["Games Played"] >= 64]
 dt_df = df[df["Position"] == "DE"]
 dt_df = dt_df[dt_df["Games Played"] >= 100]
 
+
 #FILTERING PASSING DATASET
 df = pd.read_csv("data/Career_Stats_Passing.csv")
 
@@ -75,8 +76,7 @@ df = pd.read_csv("data/Career_Stats_Passing.csv")
 df.replace('--', pd.NA, inplace=True)
 
 numeric_cols = [
-    'Games Played', 'Passes Attempted', 'Passes Completed',
-    'Passing Yards', 'TD Passes', 'Ints'
+    'Games Played', 'Passes Attempted', 'Passes Completed', 'Passing Yards', 'TD Passes', 'Ints'
 ]
 df[numeric_cols] = df[numeric_cols].apply(pd.to_numeric, errors='coerce')
 
@@ -94,5 +94,49 @@ df['Int Rate'] = (df['Ints'] / df['Passes Attempted']) * 100
 # Replace any infinite or NaN results from division by zero
 df.replace([np.inf, -np.inf], np.nan, inplace=True)
 
+#filtering out names that are in the hall of fame
 df = df[~df["Name"].isin(hof_names)]
-print(set(df['Position']))
+
+# Keep only rows where Position is 'QB' or NaN
+df = df[df['Position'].isna() | (df['Position'] == 'QB')]
+# Replace NaN with 'QB'
+df['Position'] = df['Position'].fillna('QB')
+
+#filtering based on games played
+qb_df = df[df["TD Passes"] >= 50]
+
+
+#FILTERING RECEIVING DATASET
+df = pd.read_csv("data/Career_Stats_Receiving.csv")
+
+# Replace '--' with NaN
+df.replace('--', pd.NA, inplace=True)
+
+# Define numeric columns
+numeric_cols = [
+    'Games Played', 'Receptions', 'Receiving Yards',
+    'Receiving TDs', 'Receptions Longer than 20 Yards',
+    'Receptions Longer than 40 Yards', 'First Down Receptions', 'Fumbles'
+]
+df[numeric_cols] = df[numeric_cols].apply(pd.to_numeric, errors='coerce')
+
+# Group by player to aggregate career totals
+df = df.groupby(['Player Id', 'Name', 'Position'], dropna=False)[numeric_cols].sum().reset_index()
+
+# Derived metrics
+df['Yards Per Reception'] = df['Receiving Yards'] / df['Receptions']
+df['Yards Per Game'] = df['Receiving Yards'] / df['Games Played']
+
+# Replace infinite/NaN
+df.replace([np.inf, -np.inf], np.nan, inplace=True)
+
+# Filter out HOF players and keep certain players
+df = df[~df["Name"].isin(hof_names)]
+df = df[df['Position'].isna() | df['Position'].isin(['TE', 'WR'])]
+df['Position'] = df['Position'].fillna('WR')
+
+wr_df = df[df["Position"] == 'WR']
+wr_df = wr_df[(wr_df["Receiving Yards"] > 3250) & (wr_df["Receiving TDs"] > 20)]
+
+te_df = df[df["Position"] == 'TE']
+te_df = te_df[(te_df["Receiving Yards"] > 1000) & (te_df["Receiving TDs"] > 5)]
